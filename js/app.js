ae0379299516772d68aecfc47b0cdf28fbe38bd3
Chris Rodgers <devdigression@gmail.com>
2017-12-31 18:30:01 -0500

    randomized questions

--- js/app.js
+++ js/app.js
@@ -4,57 +4,130 @@ const quizQuestions = {
 	{
 		question: `What is the oldest continuously-inhabited city founded by Europeans in the region that would become the United States?`, 
 		answers: [`Plymouth, Massachusetts`, `Philadelphia, Pennsylvania`, `Jamestown, Virginia`, `St. Augustine, Florida`],
-		correctAnswer: `St. Augustine, Florida`
+		correctAnswer: `St. Augustine, Florida`,
+		previouslyChosen: false
 	}, 
 	{
 		question: `Who wrote the pamphlet <em>Common Sense</em>, which influenced the colonists' movement for independence from Britain?`,
 		answers: [`Benjamin Franklin`, `Thomas Paine`, `John Locke`, `Thomas Jefferson`],
-		correctAnswer: `Thomas Paine`
+		correctAnswer: `Thomas Paine`,
+		previouslyChosen: false
 	},
 	{
 		question: `Which of the following colonies was NOT founded by religious dissidents?`,
 		answers: [`Rhode Island`, `Pennsylvania`, `Virginia`, `Maryland`],
-		correctAnswer: `Virginia`
+		correctAnswer: `Virginia`,
+		previouslyChosen: false
 	},
 	{
 		question: `Who became President when Abraham Lincoln was assassinated?`,
 		answers: [`Andrew Johnson`, `James Buchanan`, `Rutherford B. Hayes`, `Hannibal Hamlin`],
-		correctAnswer: `Andrew Johnson`
+		correctAnswer: `Andrew Johnson`,
+		previouslyChosen: false
 	},
 	{
 		question: `What was the purpose of the Seneca Falls Convention of 1848?`,
 		answers: [`to address the rights of women`, `to demand an immediate end to slavery`, `to contest the election of James K. Polk`, `to sign a treaty ending the Mexican-American War`],
-		correctAnswer: `to address the rights of women`
+		correctAnswer: `to address the rights of women`,
+		previouslyChosen: false
 	},
 	{
 		question: `The "Shot Heard 'Round the World" in Ralph Waldo Emerson's poem refers to`,
 		answers: [`the beginning of the land grab in the Oklahoma territory`, `the assassination of Abraham Lincoln`, `the firing at Fort Sumter`, `the battle fought at Lexington and Concord`],
-		correctAnswer: `the battle fought at Lexington and Concord`
+		correctAnswer: `the battle fought at Lexington and Concord`,
+		previouslyChosen: false
 	},
 	{
 		question: `Which of the following was NOT a major business magnate of the Gilded Age?`,
 		answers: [`John D. Rockefeller`, `Rutherford B. Hayes`, `John P. Morgan`, `Andrew Carnegie`],
-		correctAnswer: `Rutherford B. Hayes`
+		correctAnswer: `Rutherford B. Hayes`,
+		previouslyChosen: false
 	},
 	{
 		question: `Which of the following was NOT a figure involved in the abolitionist movement?`,
 		answers: [`Frederick Douglass`, `Sojourner Truth`, `John C. Calhoun`, `William Lloyd Garrison`],
-		correctAnswer: `John C. Calhoun`
+		correctAnswer: `John C. Calhoun`,
+		previouslyChosen: false
 	},
 	{
 		question: `Which figure was most closely aligned with education reform?`,
 		answers: [`William Howard Taft`, `Frances Willard`, `Horace Mann`, `Dorothea Dix`],
-		correctAnswer: `Horace Mann`
+		correctAnswer: `Horace Mann`,
+		previouslyChosen: false
 	},
 	{
 		question: `Which figure ran as both a Populist Party and Democratic Party candidate for President in the Gilded Age?`,
 		answers: [`William Howard Taft`, `Franklin Pierce`, `William Jennings Bryan`, `Grover Cleveland`],
-		correctAnswer: `William Jennings Bryan`
+		correctAnswer: `William Jennings Bryan`,
+		previouslyChosen: false
+	},
+	{
+		question: `Which event most immediately prompted the US to join World War I?`,
+		answers: [`the Lusitania was sunk by German submarines`, `the Zimmermann Telegraph was intercepted and decoded`, `Russia withdrew from the conflict following the Bolshevik Revolution`, `Woodrow Wilson won the election of 1916`],
+		correctAnswer: `the Zimmermann Telegraph was intercepted and decoded`,
+		previouslyChosen: false
+	},
+	{
+		question: `Which of the following was NOT a major business magnate of the Gilded Age?`,
+		answers: [`John D. Rockefeller`, `Rutherford B. Hayes`, `John P. Morgan`, `Andrew Carnegie`],
+		correctAnswer: `Rutherford B. Hayes`,
+		previouslyChosen: false
+	},
+	{
+		question: `Which of the following social programs begun during the Great Depression which still exists?`,
+		answers: [`Social Security`, `Civilian Conservation Corp`, `Works Progress Administration`, `Environmental Protection Agency`],
+		correctAnswer: `Social Security`,
+		previouslyChosen: false
+	},
+	{
+		question: `Who is the author of the book <em>Silent Spring</em>, which helped launch the Environmental Movement? `,
+		answers: [`Rachel Carson`, `Cesar Chavez`, `Upton Sinclair`, `Betty Friedan`],
+		correctAnswer: `Rachel Carson`,
+		previouslyChosen: false
+	},
+	{
+		question: `Who was the first student to be admitted to the segregated University of Mississippi?`,
+		answers: [`Martin Luther King, Jr.`, `Medgar Evers`, `Harriet Beecher Stowe`, `James Meredith`],
+		correctAnswer: `James Meredith`,
+		previouslyChosen: false
+	},
+	{
+		question: `Who was the first woman to serve in the Presidential Cabinet? `,
+		answers: [`Eleanor Roosevelt`, `Frances Perkins`, `Geraldine Ferraro`, `Elizabeth Stanton`],
+		correctAnswer: `Frances Perkins`,
+		previouslyChosen: false
+	},
+	{
+		question: `The US military was defeated by Native American forces at which of the following battles?`,
+		answers: [`Sand Creek`, `Wounded Knee`, `Little Bighorn`, `Black Hills`],
+		correctAnswer: `Little Bighorn`,
+		previouslyChosen: false
+	},
+	{
+		question: `Which event brought the US closest to nuclear war?`,
+		answers: [`the Korean War`, `the Cuban Missile Crisis`, `the invasion of Iraq`, `the Soviet Sputnik mission`],
+		correctAnswer: `the Cuban Missile Crisis`,
+		previouslyChosen: false
+	},
+	{
+		question: `Which US President served more than two terms in office?`,
+		answers: [`Grover Cleveland`, `William Howard Taft`, `George Washington`, `Franklin Roosevelt`],
+		correctAnswer: `Franklin Roosevelt`,
+		previouslyChosen: false
+	},
+	{
+		question: `Prior to the ratification of the Constitution, which document outlined the governmental structure of the United States?`,
+		answers: [`Bill of Rights`, `Declaration of Independence`, `Articles of Confederation`, `Continental Congress`],
+		correctAnswer: `Articles of Confederation`,
+		previouslyChosen: false
 	}
 	],
 	currentQuestion: 0,
 	correctCount: 0
 };
+
+const questionTotal = 10;
+let questionNumber;
 
 function startQuiz () {
 	$("#start-button").on("click", function(){
@@ -74,18 +147,23 @@ function nextQuestion () {
 }
 
 function generateQuestion () {
+	questionNumber = Math.floor(Math.random() * quizQuestions.questions.length);
+	console.log(quizQuestions.questions[questionNumber].correctAnswer);
+	if (quizQuestions.questions[questionNumber].previouslyChosen) {
+		generateQuestion();
+	}
 	nextQuestion();
 	$("input").on("click", function() {
 	$(".answer-choice").removeClass("selected");
 	$(this).closest(".answer-choice").addClass("selected");
 }); 
-	$("#questionText").html(quizQuestions.questions[quizQuestions.currentQuestion].question);
+	$("#questionText").html(quizQuestions.questions[questionNumber].question);
 	$("#question-form label").each(function(index) {
-	$(this).find("input").attr("value", quizQuestions.questions[quizQuestions.currentQuestion].answers[index]);
+	$(this).find("input").attr("value", quizQuestions.questions[questionNumber].answers[index]);
 	$(this).find("input").prop("checked", false);
-	$(this).find(".answerChoice").text(quizQuestions.questions[quizQuestions.currentQuestion].answers[index]);
+	$(this).find(".answerChoice").text(quizQuestions.questions[questionNumber].answers[index]);
 });
-	$("#question-count").html(`${quizQuestions.currentQuestion + 1} / ${quizQuestions.questions.length}`);
+	$("#question-count").html(`${quizQuestions.currentQuestion + 1} / ${questionTotal}`);
 }
 
 function handleSubmit () {
@@ -98,7 +176,7 @@ function handleSubmit () {
 }
 
 function checkAnswer (answer) {  
-	let isCorrect = quizQuestions.questions[quizQuestions.currentQuestion].correctAnswer;
+	let isCorrect = quizQuestions.questions[questionNumber].correctAnswer;
 	if (answer === isCorrect) {
 	$("input").closest(":checked").parent().removeClass("selected");
 	$("input").closest(":checked").parent().addClass("showCorrect");
@@ -109,6 +187,7 @@ function checkAnswer (answer) {
 	$("form").find("input[value='"+isCorrect+"']").parent().addClass("showCorrect");
 	}
 	quizQuestions.currentQuestion++;
+	quizQuestions.questions[questionNumber].previouslyChosen = true;
 	newQuestion();
 }
 
@@ -121,7 +200,7 @@ function newQuestion () {
 	$(".answer-choice").removeClass("showCorrect");
 	$(".answer-choice").removeClass("showIncorrect");
 	$("#feedback").find("h2").text("");
-	if (quizQuestions.currentQuestion === quizQuestions.questions.length) {
+	if (quizQuestions.currentQuestion === questionTotal) {
 	showResults();
 	} else {
 	generateQuestion();
@@ -133,8 +212,8 @@ function showResults() {
 	$("#question-page").addClass("hidden");
     $("#results-page").removeClass("hidden");
 	$("#results-page").find("h1").html(`
-		${quizQuestions.correctCount} out of ${quizQuestions.questions.length} questions correct!`);
-	if (quizQuestions.correctCount === quizQuestions.questions.length) {
+		${quizQuestions.correctCount} out of ${questionTotal} questions correct!`);
+	if (quizQuestions.correctCount === questionTotal) {
 	$("#results-page").find("h2").html(`Perfect score! You are a true historian!`);
 	} else if (quizQuestions.correctCount > 6) {
 	$("#results-page").find("h2").html(`Nice work! You know your U.S. History!`);
@@ -157,6 +236,9 @@ function resetQuiz () {
 function resetScore () {
 	quizQuestions.currentQuestion = 0;
 	quizQuestions.correctCount = 0;
+	for (let i = 0; i < quizQuestions.questions.length; i++) {
+		quizQuestions.questions[i].previouslyChosen = false;
+	}
 }
 
 function handleQuiz() {
